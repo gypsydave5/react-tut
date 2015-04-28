a8d227b25f2f4fcabd3f36fb1169ead83921afed
David Wickes <dave.wickes@gmail.com>
2015-04-28 22:25:15 +0100

    Marked added - starts to pass data through attributes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,27 @@
 var React = require('react');
+var Marked = require('marked');
+
+var Comment = React.createClass({
+  render: function () {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
 
 var CommentList = React.createClass({
   render: function () {
     return (
-      <div className="commentBox">
-      Hello, world! I am a comment list
+      <div className="commentList">
+        <Comment author="Pete Hunt">This is a comment</Comment>
+        <Comment author="Jordan Walke">Thir is *another* comment</Comment>
       </div>
-      );
+    );
   }
 });
 
@@ -14,9 +29,9 @@ var CommentForm = React.createClass({
   render: function () {
     return (
       <div className="commentBox">
-      Hello, world! I am a comment form
+        Hello, world! I am a comment form
       </div>
-      );
+    );
   }
 });
 
@@ -28,7 +43,7 @@ var CommentBox = React.createClass({
       <CommentList />
       <CommentForm />
       </div>
-      );
+    );
   }
 });
 
