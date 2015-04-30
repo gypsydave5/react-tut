72e53e09e1b55c3dcfe504338bd93031da96ed63
David Wickes <dave.wickes@gmail.com>
2015-04-30 21:37:04 +0100

    It's got a form, it intercepts the submit event, it clears fields

--- src/main.js
+++ src/main.js
@@ -39,11 +39,25 @@ var CommentList = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var author = React.findDOMNode(this.refs.author).value.trim();
+    var text = React.findDOMNode(this.refs.text).value.trim();
+    if (!text || !author) {
+      return;
+    }
+    // - send the comment to the server aroundabout here...
+    React.findDOMNode(this.refs.author).value = '';
+    React.findDOMNode(this.refs.text).value = '';
+    return;
+  },
   render: function () {
     return (
-      <div className="commentBox">
-        Hello, world! I am a comment form
-      </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}>
+        <input type="text" placeholder="Your name" ref="author" />
+        <input type="text" placeholder="Say something" ref="text" />
+        <input type="submit" value="Post" />
+      </form>
     );
   }
 });
